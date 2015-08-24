fa3cf3e59929f801df3e5e099b592d93889aef27
BrayanGc <brayansucos_57@hotmail.com>
2015-08-24 18:36:50 -0500

    Agregue Practicas
    agregue las practicas de la materia #WSQ03

---
+++ Suma.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+
+int main () {
+
+  int a= 0, b= 0;
+  cout << "Introdicir un numero a\n";
+  cin >> a;
+  cout << "Introdicir un numero b\n";
+  cin >> b;
+  cout << "Tu resultado es = " << a + b <<endl;
+  return 0;
+
+}
---
+++ division.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int a = 0, b = 0;
+	cout << "Introducir numero a\n";
+	cin >> a;
+	cout << "Introducir numero b\n";
+	cin >> b;
+	cout << "Tu resultado es = " << a / b << "\n";
+	return 0;
+
+}
