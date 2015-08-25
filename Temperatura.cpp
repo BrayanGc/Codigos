71ad050fcde78bed5698785173dcb12a40730fb7
BrayanGc <brayansucos_57@hotmail.com>
2015-08-24 21:03:29 -0500

    Temp
    cambio el resultado

--- Temperatura.cpp
+++ Temperatura.cpp
@@ -5,12 +5,21 @@ using namespace std;
 int main () {
 
   int a= 0, b= 5 * (a - 32) / 9;
-  cout << "Introduce temperatura en Farenheit ";
+  cout << "Introduce temperatura en Farenheit\n";
   cin >> a;
-  cout << "Tu resultado es: " << 5 * (a - 32) / 9 << " grados celcius.";
-  if (b < 100)
-  cout << " Por lo tanto el agua no hierve a esta temperatura." << endl;
+  cout << "Tu resultado es: " << 5 * (a - 32) / 9 << " grados celcius,";
 
+  if (a > 213) {
+    cout << " Por lo tanto el agua si hierve a esta temperatura.\n";
+  }
+
+  else if (a = 213, 212) {
+    cout << " Por lo tanto el agua si hierve a esta temperatura.\n";
+  }
+
+  else if (a < 212) {
+    cout << " Por lo tanto el agua no hierve a esta temperatura.\n" << endl;
+  }
   return 0;
 
 }
