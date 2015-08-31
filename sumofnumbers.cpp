b47e37612377cc44decd04e9142bc9c7cffdde73
BrayanGc <brayansucos_57@hotmail.com>
2015-08-31 09:03:23 -0500

    practica 7

--- sumofnumbers.cpp
+++ sumofnumbers.cpp
@@ -7,17 +7,14 @@ int main (){
    cout << "Suma de los dos numeros entre dos numeros" << endl;
    cout << "Introduce el primer numero\n";
    cin >> x;
-   cout << "Introduce el segundo numero\n"
+   cout << "Introduce el segundo numero\n";
    cin >> y;
 
-  while {(x <= y-1)
-    (z = z + x)
-    (x = x+1 )
-  cout << "Total= " << (z+x);
+  while (x <= y-1) {
+    (z = z + x);
+    (x = x+1 ); }
+  cout << "Total= " << (z+x) << "\n";
   return 0;
-  }
-
-
 
 
 }
